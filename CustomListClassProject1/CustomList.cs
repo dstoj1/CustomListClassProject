49cc3a919afd13f342b31b9f9eb18ac921d34fd6
Ljiljana <dstoj1@yahoo.comgit config --global user.email dstoj1@yahoo.com>
2017-08-18 11:14:28 -0500

    completed indexer

--- CustomListClassProject1/CustomList.cs
+++ CustomListClassProject1/CustomList.cs
@@ -8,5 +8,23 @@ namespace CustomListClassProject1
 {
     public class CustomList<T>
     {
+        T[] item = new T[0];
+        int count;
+        public CustomList()
+        {
+            this.count = count;
+        }   
+        public T this[int index]
+        {
+            get
+            {
+                return item[index];
+            }
+            set
+            {
+                item[index] = value;
+            }
+        }  
+        public void Add    
     }
 }
